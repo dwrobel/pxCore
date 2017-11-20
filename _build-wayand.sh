0b5353cd9fa645f8f3f77d4f64fb0c052977c49f
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2017-11-20 14:02:41 +0100

    add build scripts

---
+++ _build-wayand.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -p build
+pushd build
+cmake \
+  -DDISABLE_DEBUG_MODE=ON \
+  -DPREFER_SYSTEM_LIBRARIES=ON \
+  -DBUILD_RTREMOTE_LIBS=ON\
+  -DBUILD_WITH_WAYLAND=ON \
+  -DBUILD_PX_TESTS=OFF \
+  -DCMAKE_CXX_FLAGS="-fno-delete-null-pointer-checks -Wno-deprecated-declarations -Wno-address -DMESA_EGL_NO_X11_HEADERS -O0 -g3 -DPXSCENE_DISABLE_WST_DECODER" ..
+time make -j$(nproc) && echo "ok"
+popd
