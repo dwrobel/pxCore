93c88cb4914c04b0571fa7143cf0f3e312cc3bc4
Bunio_FH <mkubiak@libertyglobal.com>
2018-03-29 17:34:41 +0200

    clean/dirty branches check

--- examples/pxScene2d/src/pxScene2d.h
+++ examples/pxScene2d/src/pxScene2d.h
@@ -751,6 +751,7 @@ protected:
   bool mRepaint;
   #ifdef PX_DIRTY_RECTANGLES
   bool mIsDirty;
+  bool mIsTreeDirty;
   pxMatrix4f mLastRenderMatrix;
   pxRect mScreenCoordinates;
   pxRect mDirtyRect;
@@ -762,6 +763,7 @@ protected:
   void setDirtyRect(pxRect* r);
   pxRect getBoundingRectInScreenCoordinates();
   pxRect convertToScreenCoordinates(pxRect* r);
+  inline void markBranchDirty();
   #endif //PX_DIRTY_RECTANGLES
 
   pxScene2d* mScene;
@@ -1173,6 +1175,9 @@ public:
   rtError suspend(const rtValue& v, bool& b);
   rtError resume(const rtValue& v, bool& b);
   
+  pxIViewContainer* getViewContainer() {
+    return mViewContainer;
+  }
 protected:
 
   static rtError getScene(int /*numArgs*/, const rtValue* /*args*/, rtValue* result, void* ctx);
@@ -1597,6 +1602,9 @@ public:
   rtError getService(const char* name, const rtObjectRef& ctx, rtObjectRef& service);
   rtError getAvailableApplications(rtString& availableApplications);
 
+  pxScriptView* getScriptView() {
+    return mScriptView;
+  }
 private:
   bool bubbleEvent(rtObjectRef e, rtRef<pxObject> t, 
                    const char* preEvent, const char* event) ;
